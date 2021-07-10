f9c07d7ae5b044414149ef9fad5ec3dc9d476b4e
xiazhongwei <1298243794@qq.com>
2021-07-10 21:11:52 +0800

    foodie-dev

--- foodie-dev-api/src/main/java/cn/mrcode/foodiedev/config/WebMvcConfig.java
+++ foodie-dev-api/src/main/java/cn/mrcode/foodiedev/config/WebMvcConfig.java
@@ -18,8 +18,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/**")
                 // 这里拦截之后，之前能访问的 swagger2 文档就无法访问到了
                 // 因为它也是第三方 jar 包提供的 html 静态文件，所以这里也需要映射下
-                .addResourceLocations("classpath:/META-INF/resources/")  // 映射 swagger2
-                .addResourceLocations("file:/Users/mrcode/Documents/GitHub/foodie-dev/tempresource/images/");  // 映射本地静态资源
+                .addResourceLocations("classpath:/META-INF/resources/")  // 映射 swagger2--->localhost:8080/doc.html
+                .addResourceLocations("file:/Users/mrcode/Documents/GitHub/foodie-dev/tempresource/images/");
+        // 映射本地静态资源 访问地址localhost:8080/+文件在服务器根目录地址，也就是images后面的地址
     }
 
     @Bean
